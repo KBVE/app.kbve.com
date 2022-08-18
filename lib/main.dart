7410f201b00a344250b5338eaf8724e1eb9b57c3
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2022-08-18 01:36:45 -0400

    Go Router

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,80 @@
 import 'package:admin/constants.dart';
 import 'package:admin/controllers/MenuController.dart';
 import 'package:admin/screens/main/main_screen.dart';
+import 'package:admin/screens/main/login.dart';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+
+// Go Router
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  MyApp({Key? key}) : super(key: key);
+
+  static const title = 'KBVE.com';
 
   @override
-  Widget build(BuildContext context) {
-    // MaterialApp
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'KBVE.com',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: bgColor,
-        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
-            .apply(bodyColor: Colors.white),
-        canvasColor: secondaryColor,
+  Widget build(BuildContext context) => MaterialApp.router(
+        routeInformationProvider: _router.routeInformationProvider,
+        routeInformationParser: _router.routeInformationParser,
+        routerDelegate: _router.routerDelegate,
+        title: title,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: bgColor,
+          textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+              .apply(bodyColor: Colors.white),
+          canvasColor: secondaryColor,
+        ),
+      );
+
+  final _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => _HomeScreen(),
       ),
-      home: MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-            create: (context) => MenuController(),
-          ),
-        ],
-        child: MainScreen(),
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => _LoginScreen(),
       ),
-    );
-  }
+    ],
+  );
+}
+
+class _HomeScreen extends StatelessWidget {
+  const _HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: const Text(MyApp.title)),
+        body: Center(
+            child: MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create: (context) => MenuController())
+          ],
+          child: MainScreen(),
+        )),
+      );
+}
+
+class _LoginScreen extends StatelessWidget {
+  const _LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: const Text(MyApp.title)),
+        body: Center(
+            child: MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create: (context) => MenuController())
+          ],
+          child: LoginScreen(),
+        )),
+      );
 }
